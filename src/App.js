55330c019a75c1ad5dc6f8d8bf180eec43a46b15
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-28 20:43:21 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function App() {
-  return <h1>hahahahahahaha</h1>;
+  return (
+    <main className="min-w-full min-h-screen font-outfit text-[18px]"></main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        "very-dark-blue-bg": "hsl(217, 54%, 11%)",
+        "very-dark-blue-card": "hsl(216, 50%, 16%)",
+        "very-dark-blue-line": "hsl(215, 32%, 27%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", sans - serif],
+      },
+    },
   },
   plugins: [],
 };
